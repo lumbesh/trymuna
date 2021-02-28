e7d1c0eccbe087fa683ca320defc9edc210ff418
lumbesh <aj.lumbes@gmail.com>
2021-02-28 20:57:43 +0800

    praktis lang

---
+++ login.js
@@ -0,0 +1,192 @@
+//confirm button
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+})
+
+
+// Blur bg on click
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".ellipse").style.filter = "blur(4px)";
+})
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".rectangle").style.filter = "blur(4px)";
+})
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".rectangle2").style.filter = "blur(4px)";
+})
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".cat").style.filter = "blur(4px)";
+})
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".login").style.filter = "blur(4px)";
+})
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".signup").style.filter = "blur(4px)";
+})
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".about").style.filter = "blur(4px)";
+})
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".capulus").style.filter = "blur(4px)";
+})
+
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".rectangle3").style.filter = "blur(4px)";
+})
+
+//close button
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+})
+
+// remove blur bg
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".ellipse").style.filter = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".rectangle").style.filter = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".rectangle2").style.filter = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".cat").style.filter = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".login").style.filter = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".signup").style.filter = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".about").style.filter = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".capulus").style.filter = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".rectangle3").style.filter = "none";
+})
+
+
+//brewing button appear
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".brewing").style.display = "block";
+})
+
+//brewing button disappear
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".login").style.display = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".signup").style.display = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+})
+
+//remove blur after brewing button appears
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".ellipse").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".rectangle").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".rectangle2").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".cat").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".login").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".signup").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".about").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".capulus").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".rectangle3").style.filter = "none";
+})
+
+// hide div
+
+window.addEventListener('mouseup', function(event){
+    var hidediv = document.querySelector(".popup");
+    if(event.target != hidediv){
+        hidediv.style.display = 'none';
+    }
+})
+
+//remove blur after brewing button appears
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".ellipse").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".rectangle").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".rectangle2").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".cat").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".login").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".signup").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".about").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".capulus").style.filter = "none";
+})
+
+document.querySelector(".confirm").addEventListener("click", function(){
+    document.querySelector(".rectangle3").style.filter = "none";
+})
---
+++ signup.js
@@ -0,0 +1,88 @@
+//confirm button
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".popup1").style.display = "flex";
+})
+
+
+// Blur bg on click
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".ellipse").style.filter = "blur(4px)";
+})
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".rectangle").style.filter = "blur(4px)";
+})
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".rectangle2").style.filter = "blur(4px)";
+})
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".cat").style.filter = "blur(4px)";
+})
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".login").style.filter = "blur(4px)";
+})
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".signup").style.filter = "blur(4px)";
+})
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".about").style.filter = "blur(4px)";
+})
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".capulus").style.filter = "blur(4px)";
+})
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".rectangle3").style.filter = "blur(4px)";
+})
+
+//close button
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".popup1").style.display = "none";
+})
+
+// remove blur bg
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".ellipse").style.filter = "none";
+})
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".rectangle").style.filter = "none";
+})
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".rectangle2").style.filter = "none";
+})
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".cat").style.filter = "none";
+})
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".login").style.filter = "none";
+})
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".signup").style.filter = "none";
+})
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".about").style.filter = "none";
+})
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".capulus").style.filter = "none";
+})
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.querySelector(".rectangle3").style.filter = "none";
+})
